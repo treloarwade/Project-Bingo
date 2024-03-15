d6b73ace44d5fde7c4630ac70dfd7fb0ace30b0f
treloarwade <treloar.wade@gmail.com>
2024-03-15 17:32:52 -0400

    bingo

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider) => Debug.Log("Bingo");
+}
